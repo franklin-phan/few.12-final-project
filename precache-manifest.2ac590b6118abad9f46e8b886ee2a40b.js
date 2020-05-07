e5173ea44d46e60226f90f9860adec81d6d31cbd
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-05-07 12:06:34 -0500

    Updates

---
+++ precache-manifest.2ac590b6118abad9f46e8b886ee2a40b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbff5463ce0e65de1b365014240bfc91",
+    "url": "/franklin-phan/sfpopos-tutorial/index.html"
+  },
+  {
+    "revision": "cf6a38e064d15a7c0065",
+    "url": "/franklin-phan/sfpopos-tutorial/static/css/main.af676ff8.chunk.css"
+  },
+  {
+    "revision": "62d889a2fa0653c15e37",
+    "url": "/franklin-phan/sfpopos-tutorial/static/js/2.89afaf65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/franklin-phan/sfpopos-tutorial/static/js/2.89afaf65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6a38e064d15a7c0065",
+    "url": "/franklin-phan/sfpopos-tutorial/static/js/main.7d414b26.chunk.js"
+  },
+  {
+    "revision": "051617e64ddc33abcb11",
+    "url": "/franklin-phan/sfpopos-tutorial/static/js/runtime-main.75e4227e.js"
+  }
+]);
